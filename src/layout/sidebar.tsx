d31d226227908b31f9cd33e7847b764f7bfed82c
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-09-25 21:03:05 +0800

    style: set overflow of root layout to hidden

--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -1,4 +1,10 @@
 export default function Sidebar() {
   // Expanded width: 300px, UnExpanded width: 80px
-  return <div className="h-auto w-[300px] bg-red-300 p-4">Sidebar Content</div>;
+  return (
+    <div className="h-auto w-[300px] bg-white px-2 py-3">
+      <div className="bg-blue-400 p-1 rounded-md">
+        <p>Dashboard Logo</p>
+      </div>
+    </div>
+  );
 }
