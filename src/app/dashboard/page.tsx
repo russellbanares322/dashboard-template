0d4d01db7804cca8ea282f39094780a183a9bb1b
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-12-14 19:55:14 +0800

    chore: add comment in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -48,7 +48,7 @@ export default function Dashboard() {
 
   return (
     <div className="px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-      {/* Total Sales */}
+      {/* Content */}
       <div>
         <p>Total Sales</p>
         <Chart />
