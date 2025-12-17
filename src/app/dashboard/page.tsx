cb0c0bd075106f5274f71295b1821607d44e8cb0
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-12-18 00:09:15 +0800

    chore: add comment in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import BarChart from "./bar-chart";
 import { PieChart } from "./pie-chart";
 
+// Display more charts
 export default function Dashboard() {
   return (
     <div className="px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
