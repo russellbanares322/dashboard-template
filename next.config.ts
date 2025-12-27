e9fd8594553c7ac8fd45c793dcaf50c0fcc4ca24
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-12-27 19:20:15 +0800

    chore: remove configs in next config file

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  redirects: async() => {
-    return [
-      {
-        source: "/dashboard",
-        destination: "/",
-        permanent: true
-      }
-    ]
-  }
-  
 };
 
 export default nextConfig;
