e417f19f96e40e35762d9b4c3519b3c254b5e6b0
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-12-27 12:38:01 +0800

    feat: use root page layout as its dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import BarChart from "@/components/charts/bar-chart";
+import { PieChart } from "@/components/charts/pie-chart";
+
+export default function Dashboard() {
+  return (
+    <div className="px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+      {/* Content */}
+      <div>
+        <p>Total Sales</p>
+        <BarChart />
+      </div>
+      <div>
+        <p>Total Sales</p>
+        <PieChart />
+      </div>
+    </div>
+  );
 }
