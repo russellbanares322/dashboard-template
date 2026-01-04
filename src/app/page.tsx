a9e20623e89cb87471d35cc645e9dad8ffc74802
russell-banares <russell.banares@abedatasolutions.com.ph>
2026-01-04 21:43:27 +0800

    chore: remove comment in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { PieChart } from "@/components/charts/pie-chart";
 export default function Dashboard() {
   return (
     <div className="px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-      {/* Content */}
       <div>
         <p>Total Sales</p>
         <BarChart />
