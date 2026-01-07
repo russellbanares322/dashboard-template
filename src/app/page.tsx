cea74d0ab738a79771c8371e2d7b478bbffe3723
russell-banares <russell.banares@abedatasolutions.com.ph>
2026-01-07 20:55:10 +0800

    feat: include line chart in dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import BarChart from "@/components/charts/bar-chart";
+import { LineChart } from "@/components/charts/line-chart";
 import { PieChart } from "@/components/charts/pie-chart";
 
 export default function Dashboard() {
-  // Display more charts
   return (
     <div className="px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
       <div>
@@ -12,6 +12,10 @@ export default function Dashboard() {
       <div>
         <p>Total Sales</p>
         <PieChart />
+      </div>
+      <div>
+        <p>Total Sales</p>
+        <LineChart />
       </div>
     </div>
   );
---
+++ src/components/charts/line-chart.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import {
+  CartesianGrid,
+  Line,
+  LineChart as ShadcnLineChart,
+  XAxis,
+} from "recharts";
+
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "@/components/ui/chart";
+
+export const description = "A line chart";
+
+const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 214 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+} satisfies ChartConfig;
+
+export function LineChart() {
+  return (
+    <ChartContainer
+      className="min-h-[200px] w-full shadow-md rounded-md"
+      config={chartConfig}
+    >
+      <ShadcnLineChart
+        accessibilityLayer
+        data={chartData}
+        margin={{
+          left: 12,
+          right: 12,
+        }}
+      >
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent hideLabel />}
+        />
+        <Line
+          dataKey="desktop"
+          type="natural"
+          stroke="var(--color-desktop)"
+          strokeWidth={2}
+          dot={false}
+        />
+      </ShadcnLineChart>
+    </ChartContainer>
+  );
+}
