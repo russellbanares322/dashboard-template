10a915fb104c1f4390a151b83279fc2553d797fc
russell-banares <russell.banares@abedatasolutions.com.ph>
2026-01-08 21:17:59 +0800

    chore: add comment in dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { LineChart } from "@/components/charts/line-chart";
 import { PieChart } from "@/components/charts/pie-chart";
 
 export default function Dashboard() {
+  // Display more charts
   return (
     <div className="px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
       <div>
